56b25359fba0ed5bee5b27791ffd4e79263e6197
Kristina <156537483+kristinaivanova777@users.noreply.github.com>
2024-07-14 13:09:56 +0300

    Login and Register

--- Avengers/obj/Release/net6.0/Avengers.AssemblyInfo.cs
+++ Avengers/obj/Release/net6.0/Avengers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-Avengers-eba77ee8-edb7-420c-95fd-ee6226722b03")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avengers")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa3b406fcbd6f2cf4f99eeb22eb097988b08c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avengers")]
