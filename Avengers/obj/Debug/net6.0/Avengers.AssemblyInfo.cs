e6537a336388313404fa607e759d2ffe342dad40
Doriana <133523673+DorianaPetkova@users.noreply.github.com>
2024-07-16 14:01:27 +0300

    UpdateRegisterLogin

--- Avengers/obj/Debug/net6.0/Avengers.AssemblyInfo.cs
+++ Avengers/obj/Debug/net6.0/Avengers.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa3b406fcbd6f2cf4f99eeb22eb097988b08c7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
