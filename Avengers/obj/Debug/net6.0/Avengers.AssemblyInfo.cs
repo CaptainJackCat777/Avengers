f45b12da4531b90422d1b19c6e01abe3a7e92ce6
Kristina <156537483+kristinaivanova777@users.noreply.github.com>
2024-07-24 18:19:36 +0300

    Design

--- Avengers/obj/Debug/net6.0/Avengers.AssemblyInfo.cs
+++ Avengers/obj/Debug/net6.0/Avengers.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fa3b406fcbd6f2cf4f99eeb22eb097988b08c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6537a336388313404fa607e759d2ffe342dad40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Avengers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
